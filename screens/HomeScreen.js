08077cdc7f7b86aedda719a83a4f4a7a56d6c996
DapiFromKazzak <aaro.kallioinen@gmail.com>
2024-09-18 19:05:17 +0300

    Tehtävä valmis

---
+++ screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import { Button } from 'react-native-paper';
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={style.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
